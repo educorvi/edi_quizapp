7eda3f6a9e9d1c38da4c2e936020cfe88cef746d
Julian <neferin42@gmail.com>
2019-09-10 10:17:43 +0200

    Verbesserungen: -Auswertungsbildschirm für Multible Choice Fragen verbessert -Neues Logo und PWA-Unterstützung für IOS

--- src/components/QuizView/Auswertung/Details.vue
+++ src/components/QuizView/Auswertung/Details.vue
@@ -4,33 +4,35 @@
             <b-card-header :header-text-variant="variant">{{frage.title}}</b-card-header>
             <b-collapse :id="'collapse_'+frage['@id']">
                 <b-card-body>
-                    <b-row>
+
+                    <!--                    Reihe-->
+                    <b-row v-if="frage.antworten[0].bewertung === 'reihe'">
                         <b-col class="border-right border-dark">
                             <div class="border-bottom border-dark">Ihre Wahl</div>
 
-                            <div v-if="frage.antworten[0].bewertung === 'reihe'">
-                                <p :key="antwort.antwort" v-for="(antwort, index) in selected">{{index+1}}:
+                            <div>
+                                <p :key="antwort.antwort+'_f'+index" v-for="(antwort, index) in selected">{{index+1}}:
                                     {{antwort.antwort}}</p>
-                            </div>
-                            <div v-else>
-                                <p :key="frage.antworten[antwort].antwort" v-for="antwort in selected">
-                                    {{frage.antworten[antwort].antwort}}</p>
                             </div>
                         </b-col>
                         <b-col class="border-left border-dark">
                             <div class="border-bottom border-dark">Richtige Lösung</div>
-
-                            <div v-if="frage.antworten[0].bewertung === 'reihe'">
-                                <p :key="antwort.antwort" v-for="(antwort, index) in frage.antworten">{{index+1}}:
+                            <div>
+                                <p :key="antwort.antwort+'_r'+index" v-for="(antwort, index) in frage.antworten">
+                                    {{index+1}}:
                                     {{antwort.antwort}}</p>
-                            </div>
-                            <div v-else>
-                                <div :key="antwort.antwort" v-for="antwort in frage.antworten">
-                                    <p v-if="antwort.bewertung==='richtig'">{{antwort.antwort}}</p>
-                                </div>
                             </div>
                         </b-col>
                     </b-row>
+
+
+                    <!--                    Multible Choice-->
+                    <div :key="antwort.antwort+'_'+index" v-else v-for="(antwort, index) in frage.antworten">
+                        <b-checkbox :checked="selected.includes(index)" :state="getCheckboxState(index)" disabled>
+                            {{antwort.antwort}}
+                        </b-checkbox>
+                    </div>
+
                 </b-card-body>
             </b-collapse>
         </b-card>
@@ -50,6 +52,34 @@
                 }
             },
         },
+        methods: {
+            getCardVariant(index) {
+                if (this.frage.antworten[index].bewertung === "richtig" && this.selected.includes(index)) {
+                    return "success";
+                }
+                if (this.frage.antworten[index].bewertung === "richtig" && !this.selected.includes(index)) {
+                    return "warning";
+                }
+                if (this.frage.antworten[index].bewertung === "falsch" && this.selected.includes(index)) {
+                    return "danger";
+                }
+
+                return null;
+            },
+            getCheckboxState(index) {
+                if (this.frage.antworten[index].bewertung === "richtig" && this.selected.includes(index)) {
+                    return true;
+                }
+                if (this.frage.antworten[index].bewertung === "richtig" && !this.selected.includes(index)) {
+                    return false;
+                }
+                if (this.frage.antworten[index].bewertung === "falsch" && this.selected.includes(index)) {
+                    return false;
+                }
+
+                return null;
+            }
+        }
     }
 </script>
 
